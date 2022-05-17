03c8e731fa57c5462b205fb1d026e23520d7e623
ThamaraHub <102432272+ThamaraHub@users.noreply.github.com>
2022-05-17 15:45:15 -0300

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -5,12 +5,14 @@ public class Empresa
     public static void main(String args[])
     {
         ListaPessoa listaCandidatos = new ListaPessoa();
-
+        
+        //chamada da lista de candidatos
         listaCandidatos.inserePessoaNaLista("João", "Silva", "1323515", 2000);
         listaCandidatos.inserePessoaNaLista("Maria", "Silva", "1323515", 1990);
         listaCandidatos.inserePessoaNaLista("Thiago", "Silva", "1323515", 1998);
         listaCandidatos.inserePessoaNaLista("Ana", "Silva", "1323515", 2005);
-
+    
+        // chamada para a impressão da lista de candidatos
         listaCandidatos.imprimeCandidatosLista();
     }
 }
